69e357198ea5837061aebbec99ca2c5b05738797
riittagerhards <rgerhards@outlook.com>
2021-10-04 19:53:49 +0200

    add favorite card

---
+++ src/components/favorite-card/favorite-card.stories.js
@@ -0,0 +1,9 @@
+import "./favorite-card.css";
+import favoriteCard from "./favorite-card.html";
+
+export default {
+  title: "Components/Favorite Card",
+  parameters: { layout: "centered" },
+};
+
+export const fruits = () => favoriteCard;
